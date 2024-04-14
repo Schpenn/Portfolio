ad306834a2ab52376f6f100234413eb4196d28cb
Espen Bjørnsen <espenbjornsen@hotmail.com>
2024-04-14 11:58:55 +0200

    modalscript og bilder

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-var modal = document.getElementById("istqb-foundation-modal");
-var btn = document.getElementsById("istqb-foundation-modal");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
+document.addEventListener("DOMContentLoaded", function() {
+  var modal = document.getElementById("istqb-foundation-modal");
+  var btn = document.getElementsByClassName("medal");
+  var medals = Array.from(btn);
+ 
+ medals.forEach(b => {
+    b.addEventListener('click', function(){
+      console.log(this )
+      openModal(this);
+    })
+  });
+  function openModal(){
     modal.style.display = "block";
   }
+  var span = document.getElementsByClassName("close")[0];
+ // btn[0].onclick = function() {
+ //   console.log(btn.findIndex);
+ //   modal.style.display = "block";
+ // }
 
   span.onclick = function() {
     modal.style.display = "none";
   }
 
   window.onclick = function(event) {
+    
     if (event.target == modal) {
       modal.style.display = "none";
     }
   }
+});
